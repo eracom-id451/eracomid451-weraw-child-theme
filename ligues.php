61365627c941ad3b79e3cce13ebb938452d6964d
Eduardo <eduardo.paradox@gmail.com>
2019-04-08 14:09:39 +0200

    Update ligues.php

--- ligues.php
+++ ligues.php
@@ -6,61 +6,61 @@ function weraw_ligues() {
     "Appengers" => array(
       array(
         "name" => "CaptainMalware", // le nom du personnage
-        "image"=>"supermachin.png", // placer l'image dans medias/vignettes, format x sur x.
+        "image"=>"HEROS-malwear.png", // placer l'image dans medias/vignettes, format x sur x.
         "url"=>"supermachin.example.com", // sans le https://
       ),
 			array(
         "name" => "Hackerman",
-        "image"=>"supermachin3.png",
+        "image"=>"HEROS-hackerman.png",
         "url"=>"supermachin3.example.com",
       ),
 			array(
         "name" => "Troisdé",
-        "image"=>"supermachin3.png",
+        "image"=>"HEROS-troisde.png",
         "url"=>"super.mariusparisod.ch",
       ),
       array(
         "name" => "Afterburner",
-        "image"=>"supermachin2.png",
+        "image"=>"HEROS-wand.png",
         "url"=>"hero.loannjuillerat.ch",
       ),
     ),
     "Doctype" => array(
       array(
         "name" => "Zindex",
-        "image"=>"supermachin3.png",
+        "image"=>"HEROS-wand.png",
         "url"=>"superheros.diano-dimatteo.ch",
       ),
 			array(
         "name" => "Bordercollapse",
-        "image"=>"supermachin3.png",
-        "url"=>"HEROS-BorderCollapse.png",
+        "image"=>"HEROS-BorderCollapse.png",
+        "url"=>"supermachin3.example.com",
       ),
 			array(
         "name" => "Opacity",
-        "image"=>"supermachin3.png",
+        "image"=>"HEROS-opacity.png",
         "url"=>"superheros.tenzinlama.ch",
       ),
       array(
         "name" => "Dropshadow",
-        "image"=>"supermachin4.png",
+        "image"=>"HEROS-wand.png",
         "url"=>"weraw.jonas-paul.ch",
       )
     ),
 		"Hybrid" => array(
 			array(
 				"name" => "Eraser",
-				"image"=>"pathfinder_hero.png",
+				"image"=>"HEROS-ereaser.png",
 				"url"=>"eraser.tenzinlama.ch",
 			),
 			array(
 				"name" => "Pathfinder",
-				"image"=>"pathfinder_hero.png",
+				"image"=>"HEROS-pathfinder.png",
 				"url"=>"superhero.tanguybula.com",
 			),
 			array(
 				"name" => "Wand",
-				"image"=>"supermachin4.png",
+				"image"=>"HEROS-wand.png",
 				"url"=>"hero.eduardo-espinoza.ch",
 			)
 		),
